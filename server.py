ae8bd064aa055bbc5de2fef99db1847de1c00977
Sergio Lazaro Mondargo <sergiolazaromondargo@gmail.com>
2023-09-21 14:13:55 -0500

    update

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect, request, jsonify
+from flask import (
+    Flask,
+    send_from_directory,
+    render_template,
+    redirect,
+    request,
+    jsonify,
+)
 from langchain.llms import CTransformers
 from langchain.chains import LLMChain
 from langchain import PromptTemplate
@@ -13,27 +20,28 @@ app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
 
-@app.route('/')
+@app.route("/static/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
+
+
+@app.route("/")
 def home():
-   return render_template('index.html')
+    return render_template("index.html")
+
 
 NOMBRE_INDICE_CHROMA = "/db2"
 
 
-
 embedding_instruct = HuggingFaceInstructEmbeddings(
-    model_name="all-mpnet-base-v2",
-    model_kwargs={"device":"cpu"}
+    model_name="all-mpnet-base-v2", model_kwargs={"device": "cpu"}
 )
 
 vectorstore_chroma = Chroma(
-    persist_directory=NOMBRE_INDICE_CHROMA,
-    embedding_function=embedding_instruct
+    persist_directory=NOMBRE_INDICE_CHROMA, embedding_function=embedding_instruct
 )
+
 
 # Loading the model
 def load_llm(max_tokens, prompt_template):
@@ -42,25 +50,46 @@ def load_llm(max_tokens, prompt_template):
 
     return llm_chain
 
-# API route to generate article
-@app.route('/search', methods=['POST'])
+
+# ...
+
+
+# API route to generate query
+@app.route("/search", methods=["POST"])
 def search():
-    if request.method == 'POST':
-        # Obtener la consulta del formulario HTML
-        query = request.form.get('query')
+    if request.method == "POST":
+        # Obtener el JSON de entrada enviado por el cliente
+        data = request.get_json()
 
-        # Realizar la búsqueda de similitud de texto en el índice Chroma
-        docs_q = vectorstore_chroma.similarity_search_with_score(query, k=1)
+        # Verificar si el JSON contiene una clave llamada 'query'
+        if "query" in data:
+            query = data["query"]
 
-        # Puedes procesar los resultados y enviarlos de vuelta al cliente como JSON, por ejemplo:
-        result = {
-            'query': query,
-            'most_similar_doc': docs_q[0] if docs_q else None  # El documento más similar (si hay resultados)
-        }
+            # Realizar la búsqueda de similitud de texto en el índice Chroma
+            docs_q = vectorstore_chroma.similarity_search_with_score(query, k=1)
 
-        return jsonify(result)
+            # Puedes procesar los resultados y enviarlos de vuelta al cliente como JSON, por ejemplo:
+            result = {
+                "query": query,
+                "most_similar_doc": docs_q[0]
+                if docs_q
+                else None,  # El documento más similar (si hay resultados)
+            }
+
+            return jsonify(result)
+        else:
+            # Si el JSON de entrada no contiene la clave 'query', retorna un mensaje de error
+            return (
+                jsonify(
+                    {
+                        "error": 'El JSON de entrada debe contener una clave llamada "query"'
+                    }
+                ),
+                400,
+            )
 
     # Manejar otras condiciones si es necesario
+
 
 # ...
 
